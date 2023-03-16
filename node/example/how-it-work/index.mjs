6b483c3f0123726832f3baa36724cb5847142d46
dmelnik <dmelnik@dengisrazy.ru>
2023-03-16 19:15:11 +0300

    event loop

--- node/example/how-it-work/index.mjs
+++ node/example/how-it-work/index.mjs
@@ -1,5 +1,11 @@
 console.log('test')
+
+setTimeout(() => console.log('setTimeout'),0)
+
 setImmediate(function immediate () {
     console.log('immediate');
 });
+
+Promise.resolve('promise').then(r => console.log(r))
+
 console.log('test2')
